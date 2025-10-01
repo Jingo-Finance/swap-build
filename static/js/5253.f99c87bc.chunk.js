8576cf0ac452a6372be906af97e14aaed391a817
Fety <warpfinanceeth@gmail.com>
2025-10-01 02:41:48 -0700

    Update

---
+++ static/js/5253.f99c87bc.chunk.js
@@ -0,0 +1,21 @@
+"use strict";(self.webpackChunk_darkflorist_horswap=self.webpackChunk_darkflorist_horswap||[]).push([[5253],{35253:(e,n,a)=>{a.r(n),a.d(n,{default:()=>g});var t=a(42893),s=a(12204),i=a(38517),o=a(60476),r=a(79214),d=a(51871),c=a.n(d),l=a(41440),p=a(87253),h=a(93589);const f=p.default.div.withConfig({displayName:"WhatIsHorswap__Container",componentId:"sc-bc6d5ff-0"})`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`,u=(0,p.default)(f).withConfig({displayName:"WhatIsHorswap__Header",componentId:"sc-bc6d5ff-1"})`
+  gap: 30px;
+`,m=(0,p.default)(f).withConfig({displayName:"WhatIsHorswap__PageWrapper",componentId:"sc-bc6d5ff-2"})`
+  flex: 1;
+  justify-content: center;
+  gap: 50px;
+  padding: 10px;
+
+  @media screen and (min-width: ${({theme:e})=>e.breakpoint.md}px) {
+    justify-content: space-between;
+    padding-top: 64px;
+  }
+`,w=p.default.div.withConfig({displayName:"WhatIsHorswap__MarkDownWrapper",componentId:"sc-bc6d5ff-3"})`
+  max-width: 1024px;
+  overflow: hidden;
+`;function g(){const e=(0,r.d)()?h.Tv.LargeHeader:h.Tv.Hero;return(0,t.jsxs)(m,{children:[(0,t.jsx)(u,{children:(0,t.jsx)(f,{children:(0,t.jsx)(e,{children:"What is Jingo?"})})}),(0,t.jsx)(o.Tz,{gap:"md",children:(0,t.jsx)(w,{children:(0,t.jsx)(c(),{source:"# Horswap is an improved Uniswap Interface\nHorswap is a fork of [Uniswap Interface v4.266.2](https://github.com/Uniswap/interface/releases/tag/v4.266.2). The version v4.266.2 is the last version without added UI fees and that would still allow users to do local routing. Horswap has then significantly improved the interface's censorship resistance and privacy.\n\nHere are the significant changes:\n- Changed Uniswap branding to Horswap branding\n- Removed Uniswap privacy policy\n- Removed all analytics queries (Uniswap interface is really noisy in reporting everything you do to their analytics system)\n- Removed support for wallet connect wallets (Unfortunately these require centralized server to function)\n- Changed socials to point to dark.florist equivalents\n- Removed copyright notices for Uniswap\n- Removed blacklisted tokens and user addresses\n- Replaced the default RPC (Infura, which censors) with Keydonix (does not censor)\n- Removed Moonpay (a centralized fiat payment processsor)\n- Removed NFT related features (unfortunately these only function by using centralized services)\n- Removed Subgraph (unfortunately this is also a centralized service)\n- Removed pages that require subgraph (mini portfolio, portfolio, NFTs, token pages, pool details, search bar etc)\n- Removed fiat currency selector (requires subgraph)\n- Removed external routing, all routing is done using the default RPC or users wallet RPC\n- Removed UniswapX (UniswapX depends on centralized servers)\n- Settings have been moved to where the mini portfolio used to be\n- Changed token pricing to be from a simulated swap with USDC, and it is shown to users that this is in USDC (not in dollars)\n- Removed claim UNI tokens popup\n- Added docker building and deployment to IPFS\n\nYou can see all the changes by [comparing Horswap to Uniswap Interface V4.266.2](https://github.com/Uniswap/interface/compare/v4.266.2...DarkFlorist:Horswap:main). You can also find the whole codebase in [GitHub](https://github.com/Jingo-Finance/).\n\t\t\t\t\t".replace(/[\n\r]/g,"\n")})})}),(0,t.jsx)(i.Hm,{as:l.rU,to:"/",children:(0,t.jsx)(s.cC,{id:"3LK7dH"})})]})}}}]);
+//# sourceMappingURL=5253.f99c87bc.chunk.js.map
